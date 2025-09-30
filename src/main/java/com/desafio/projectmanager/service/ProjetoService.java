8cec3eae6a96534152daba29d24051fbc944dd14
ragevoid <riccardo.escorche@gmail.com>
2025-09-29 21:20:36 -0300

    Alteração da classe de ProjetoService para garantir codigo limpo, separação de responsabilidades e definição de classes constantes para regras de negocio referente a valores de risco e prazo em meses

--- src/main/java/com/desafio/projectmanager/service/ProjetoService.java
+++ src/main/java/com/desafio/projectmanager/service/ProjetoService.java
@@ -1,6 +1,7 @@
 package com.desafio.projectmanager.service;
 
-import java.time.LocalDateTime;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.UUID;
@@ -8,9 +9,13 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.desafio.projectmanager.dto.request.ProjetoRequestDTO;
+import com.desafio.projectmanager.dto.response.ProjetoDetalhesDTO;
+import com.desafio.projectmanager.dto.response.ProjetoResumoDTO;
 import com.desafio.projectmanager.handler.exceptions.BusinessException;
-import com.desafio.projectmanager.model.projeto.ClassificacaoRisco;
+import com.desafio.projectmanager.mapper.ProjetoMapper;
 import com.desafio.projectmanager.model.projeto.Projeto;
+import com.desafio.projectmanager.model.projeto.Risco;
 import com.desafio.projectmanager.model.projeto.StatusProjeto;
 import com.desafio.projectmanager.repository.ProjetoRepository;
 
@@ -20,66 +25,84 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ProjetoService {
 
-    private final ProjetoRepository projetoRepository;
+     private final ProjetoRepository projetoRepository;
+    private final ProjetoMapper projetoMapper; 
 
-    public List<Projeto> listarProjetos() {
+    private static final BigDecimal ORCAMENTO_RISCO_BAIXO_LIMITE = new BigDecimal("100000.00");
+    private static final BigDecimal ORCAMENTO_RISCO_ALTO_INICIO = new BigDecimal("500000.00");
+    private static final long PRAZO_RISCO_BAIXO_MESES = 3;
+    private static final long PRAZO_RISCO_ALTO_MESES = 6;
+
+
+    public List<ProjetoResumoDTO> listarProjetos() {
         return projetoRepository.findAllByDeletedFalse().stream()
+                .map(projeto -> {
+                    Risco risco = calcularRisco(projeto);
+                    return projetoMapper.toResumoDTO(projeto, risco); 
+                })
                 .collect(Collectors.toList());
     }
 
-    public Projeto eliminarProjeto(UUID projetoId) {
-        Projeto projeto = encontrarPorId(projetoId);
+    public ProjetoDetalhesDTO encontrarPorId(UUID projetoId) {
+        Projeto projeto = projetoRepository.findByIdAndDeletedFalse(projetoId)
+                .orElseThrow(() -> new IllegalArgumentException("Projeto não encontrado com ID: " + projetoId));
+
+        Risco risco = calcularRisco(projeto);
+        
+        return projetoMapper.toDetalhesDTO(projeto, risco);
+    }
+    
+    public ProjetoDetalhesDTO salvarProjeto(ProjetoRequestDTO projetoDTO) {
+ 
+        Projeto projeto = projetoMapper.toEntity(projetoDTO);
+        Projeto projetoSalvo = projetoRepository.save(projeto);
+        Risco risco = calcularRisco(projetoSalvo);
+        return projetoMapper.toDetalhesDTO(projetoSalvo, risco);
+    }
+
+    public void eliminarProjeto(UUID projetoId) {
+        Projeto projeto = projetoRepository.findByIdAndDeletedFalse(projetoId)
+            .orElseThrow(() -> new IllegalArgumentException("Projeto não encontrado com ID: " + projetoId));
+
         StatusProjeto status = projeto.getStatus();
 
-        if (status == StatusProjeto.INICIADO || status == StatusProjeto.EM_ANDAMENTO
-                || status == StatusProjeto.ENCERRADO) {
-            throw new BusinessException("Projeto não pode ser eliminado, Verifique Status");
+        if (status == StatusProjeto.INICIADO || status == StatusProjeto.EM_ANDAMENTO || status == StatusProjeto.ENCERRADO) {
+            throw new BusinessException("Projeto com status '" + status + "' não pode ser excluído.");
         }
+        
         projeto.setDeleted(true);
-        return projetoRepository.save(projeto);
+        projetoRepository.save(projeto);
     }
 
-    public Projeto salvarProjeto(Projeto projeto) {
-        ClassificacaoRisco risco = validarRisco(projeto);
-        projeto.setClassificacaoRisco(risco);
-        return projetoRepository.save(projeto);
-    }
+    private Risco calcularRisco(Projeto projeto) {
+        long mesesTotais = calcularMesesTotais(projeto);
+        BigDecimal orcamento = projeto.getOrcamento();
 
-    public Projeto encontrarPorId(UUID projetoId) {
-        return projetoRepository.findByIdAndDeletedFalse(projetoId)
-                .orElseThrow(() -> new IllegalArgumentException("Projeto não encontrado con ID: " + projetoId));
-    }
-
-
-    public ClassificacaoRisco validarRisco(Projeto projeto) {
-
-        long mesesTotais = CalcularMesesTotais(projeto);
-        double orcamentoDouble = projeto.getOrcamento().doubleValue();
-        if (esRiscoAlto(mesesTotais, orcamentoDouble)) {
-            return ClassificacaoRisco.ALTO;
+        if (esRiscoAlto(mesesTotais, orcamento)) {
+            return Risco.ALTO;
         }
-        if (esRiscoBajo(mesesTotais, orcamentoDouble)) {
-            return ClassificacaoRisco.BAIXO;
+        if (esRiscoBaixo(mesesTotais, orcamento)) {
+            return Risco.BAIXO;
         }
-        return ClassificacaoRisco.MEDIO;
+        return Risco.MEDIO;
     }
 
-    public long CalcularMesesTotais(Projeto projeto) {
-        LocalDateTime inicio = projeto.getDataInicio();
-        LocalDateTime finPrevisto = projeto.getDataFinalPrevisao();
-        return ChronoUnit.MONTHS.between(inicio, finPrevisto);
+    private long calcularMesesTotais(Projeto projeto) {
+        LocalDate inicio = projeto.getDataInicio();
+        LocalDate fimPrevisto = projeto.getDataFinalPrevisao();
+        return ChronoUnit.MONTHS.between(inicio, fimPrevisto);
     }
 
-    public boolean esRiscoBajo(Long mesesTotais, double orcamento) {
-        boolean verificaMeses = mesesTotais <= 3;
-        boolean verificaOrcamento = orcamento < 100001;
-        return verificaMeses && verificaOrcamento;
+    private boolean esRiscoBaixo(long mesesTotais, BigDecimal orcamento) {
+        boolean prazoOk = mesesTotais <= PRAZO_RISCO_BAIXO_MESES;
+        boolean orcamentoOk = orcamento.compareTo(ORCAMENTO_RISCO_BAIXO_LIMITE) <= 0;
+        return prazoOk && orcamentoOk;
     }
 
-    public boolean esRiscoAlto(Long mesesTotais, double orcamento) {
-        boolean verificaMeses = mesesTotais > 6;
-        boolean verificaOrcamento = orcamento > 500000;
-        return verificaMeses || verificaOrcamento;
+    private boolean esRiscoAlto(long mesesTotais, BigDecimal orcamento) {
+        boolean prazoOk = mesesTotais > PRAZO_RISCO_ALTO_MESES;
+        boolean orcamentoOk = orcamento.compareTo(ORCAMENTO_RISCO_ALTO_INICIO) > 0;
+        return prazoOk || orcamentoOk;
     }
 
 }
